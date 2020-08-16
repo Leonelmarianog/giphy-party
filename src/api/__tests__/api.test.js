e7d3d5eed3c48353017689595aa3be89974417d0
Leonel Gauna <leonel.mariano.g@gmail.com>
2020-08-15 22:39:28 -0300

    Add tests for API module

---
+++ src/api/__tests__/api.test.js
@@ -0,0 +1,55 @@
+import getGifs from '../api.js';
+import dogGifsFixture from './dogGifsFixture.json';
+
+describe('getGifs', () => {
+  beforeEach(() => {
+    global.fetch = jest.fn();
+  });
+
+  it('Returns an array with data when the request is OK', () => {
+    global.fetch.mockImplementationOnce(
+      () =>
+        new Promise((resolve) => {
+          resolve({
+            ok: true,
+            json: () => dogGifsFixture,
+          });
+        })
+    );
+    const searchTerm = 'dog';
+    return expect(getGifs(searchTerm)).resolves.toBeInstanceOf(Array);
+  });
+
+  it('Throws an error when the request fails', () => {
+    global.fetch.mockImplementationOnce(
+      () =>
+        new Promise((reject) => {
+          reject({ ok: false });
+        })
+    );
+    const searchTerm = 'dog';
+    return expect(getGifs(searchTerm)).rejects.toThrow(
+      'Something happened, please try again in a few minutes!.'
+    );
+  });
+
+  it('Throw an error when there is no data that matches the search term', () => {
+    global.fetch.mockImplementationOnce(
+      () =>
+        new Promise((resolve) => {
+          resolve({
+            ok: true,
+            json: () => {
+              return {
+                data: [],
+              };
+            },
+          });
+        })
+    );
+    const searchTerm = 'zxcvbnmasdfghjklñ';
+    return expect(getGifs(searchTerm)).rejects.toThrow(
+      "Can't find any gifs that matches the search term."
+    );
+  });
+});
