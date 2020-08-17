7beb4439d8411a7bb2bba92a5f7e4a95980d3622
Leonel Gauna <leonel.mariano.g@gmail.com>
2020-08-17 17:28:29 -0300

    Update services.test.js

--- src/services/__tests__/services.test.js
+++ src/services/__tests__/services.test.js
@@ -1,22 +1,56 @@
-import fs from 'fs';
 import findGifs from '../services.js';
-/* import getGifsUrls from '../../api/api.js';
-import { addNewGif, updateHelper } from '../../ui/ui.js'; */
+import fs from 'fs';
 
-describe('findGifs', () => {
-  it('Displays a gif in the webpage if it finds it', async () => {
-    const searchQuery = 'dog';
+describe('findGifs', async () => {
+  it('calls getGifsUrls, updateHelper and addNewGif when request is OK', async () => {
     const event = { preventDefault: () => {} };
-
-    document.body.innerHTML = fs.readFileSync(
-      'src/services/__tests__/fixtures/index.html'
+    const mockGetGifsUrls = jest.fn(
+      () =>
+        new Promise((resolve) => {
+          resolve([]);
+        })
     );
+    const mockUpdateHelper = jest.fn();
+    const mockAddNewGif = jest.fn();
+    const searchQuery = 'dog';
+
+    document.body.innerHTML = fs.readFileSync('index.html');
 
     document.querySelector('#search-query').value = searchQuery;
 
-    const getGifsUrls = jest.fn();
+    await findGifs(event, mockGetGifsUrls, mockAddNewGif, mockUpdateHelper);
 
-    await findGifs(event, getGifsUrls);
-    expect(getGifsUrls.mock.calls.length).toBe(1);
+    expect(mockGetGifsUrls).toBeCalledWith(searchQuery);
+    expect(mockGetGifsUrls.mock.calls.length).toBe(1);
+    expect(mockUpdateHelper).toBeCalledWith('');
+    expect(mockUpdateHelper.mock.calls.length).toBe(1);
+    expect(mockAddNewGif.mock.calls[0][0]).toBeInstanceOf(Array);
+    expect(mockAddNewGif.mock.calls.length).toBe(1);
+  });
+
+  it('Only calls updateHelper when request is not OK', async () => {
+    const event = { preventDefault: () => {} };
+    const error = 'The Error';
+    const mockGetGifsUrls = jest.fn(
+      () =>
+        new Promise(() => {
+          throw new Error(error);
+        })
+    );
+    const mockUpdateHelper = jest.fn();
+    const mockAddNewGif = jest.fn();
+    const searchQuery = 'asdfghjklñ';
+
+    document.body.innerHTML = fs.readFileSync('index.html');
+
+    document.querySelector('#search-query').value = searchQuery;
+
+    await findGifs(event, mockGetGifsUrls, mockAddNewGif, mockUpdateHelper);
+
+    expect(mockGetGifsUrls).toBeCalledWith(searchQuery);
+    expect(mockGetGifsUrls.mock.calls.length).toBe(1);
+    expect(mockAddNewGif.mock.calls.length).toBe(0);
+    expect(mockUpdateHelper.mock.calls[0][0]).toBe(error);
+    expect(mockUpdateHelper.mock.calls.length).toBe(1);
   });
 });
