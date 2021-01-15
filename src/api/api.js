eafbb8fe8e4dc1fbf096cd9bbd2c76aef3f617e4
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-01-15 19:56:15 -0300

    Adds data validation for api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,10 @@
-export default async function fetchData(searchQuery) {
-  // yes, I know storing the api key here is a terrible idea, but hey, we are just requesting GIFS to a public API,
-  // not some super important and private information from a database
+export default async function getGif(searchTerm) {
+  if (!searchTerm) {
+    throw new Error('A search term is needed in order to get a gif.');
+  }
+
   const response = await fetch(
-    `https://api.giphy.com/v1/gifs/search?api_key=dyhxkzTTwEEMUNuC5zjFX5Hzt6bsClFU&q=${searchQuery}&limit=25&offset=0&rating=g&lang=en`
+    `https://api.giphy.com/v1/gifs/search?api_key=dyhxkzTTwEEMUNuC5zjFX5Hzt6bsClFU&q=${searchTerm}&limit=25&offset=0&rating=g&lang=en`
   );
 
   if (!response.ok) {
