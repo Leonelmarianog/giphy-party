1fb8eef8ef21faaa416eeb030d80a815bcd7438e
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-01-15 19:56:48 -0300

    Updates services.js

--- src/services/services.js
+++ src/services/services.js
@@ -1,11 +1,25 @@
-export default async function getGifsData(
-  fetchDataCallback,
-  updateHelperCallback
-) {
-  const searchQuery = document.querySelector('#search-query').value;
-  try {
-    return await fetchDataCallback(searchQuery);
-  } catch (err) {
-    updateHelperCallback(err.message);
+import getGifFromAPI from '../api/api.js';
+
+export default async function getGif(searchTerm) {
+  if (!searchTerm) {
+    throw new Error('A search term is needed in order to get a gif.');
   }
+
+  let gifUrl = null;
+
+  try {
+    const gifsData = await getGifFromAPI(searchTerm);
+
+    if (gifsData.data.length === 0) {
+      throw new Error('No results.');
+    }
+
+    const gifsUrls = gifsData.data.map((obj) => obj.images.original.url);
+    const randomIndex = Math.floor(Math.random() * gifsUrls.length);
+    gifUrl = gifsUrls[randomIndex];
+  } catch (error) {
+    throw error;
+  }
+
+  return gifUrl;
 }
