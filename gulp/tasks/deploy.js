79ad2390b8542b48aeafdd4604db389b5e390a90
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-01-18 17:46:51 -0300

    Update deploy.js

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -1,9 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const { src } = require('gulp');
 const ghPages = require('gulp-gh-pages');
+const { execSync } = require('child_process');
+
+function getGitCommitHash() {
+  return execSync('git rev-parse HEAD').toString().trim();
+}
 
 function deploy() {
-  return src('./dist/**/*').pipe(ghPages());
+  return src('./dist/**/*').pipe(
+    ghPages({ message: `Deploy to GitHub Pages: ${getGitCommitHash()} --skip-ci` })
+  );
 }
 
 module.exports = deploy;
